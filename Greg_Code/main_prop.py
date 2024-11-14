f894cacc72e891f0f354f38ad6546a6cf2a0f00a
swagert1 <gmichalak1982@gmail.com>
2024-11-13 21:39:36 -0600

    Greg code updates 11/13/2024

--- Greg_Code/main_prop.py
+++ Greg_Code/main_prop.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
+from sqlalchemy import Integer, String, Float, text
+import pandas as pd
 
 '''
 Red underlines? Install the required packages first: 
@@ -26,7 +27,7 @@ class Base(DeclarativeBase):
 
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///C:/Users/gmich/day-63-library-project-end/All_Properties.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///C:/Users/gmich/property-vacancies/Greg_Code/All_Properties.db"
 
 # Create the extension
 db = SQLAlchemy(model_class=Base)
@@ -34,8 +35,13 @@ db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 
-# CREATE TABLE
-class Book(db.Model):
+# CREATE TABLE - the class name must match the name of the table in the SQL database
+class house(db.Model):
+    id: Mapped[str] = mapped_column(String(20), primary_key=True)
+    SQFT: Mapped[float] = mapped_column(Float, nullable=True)
+    Market_Add: Mapped[float] = mapped_column(Float, nullable=True)
+
+class Prop(db.Model):
     id: Mapped[str] = mapped_column(String(20), primary_key=True)
     SQFT: Mapped[float] = mapped_column(Float, nullable=True)
     Market_Add: Mapped[float] = mapped_column(Float, nullable=True)
@@ -45,58 +51,85 @@ class Book(db.Model):
 with app.app_context():
     db.create_all()
 
+df = pd.read_excel('Available_Properties.xlsx')
+
+prop_list = df['id'].tolist()
+
 
 @app.route('/')
 def home():
+
     # READ ALL RECORDS
     # Construct a query to select from the database. Returns the rows in the database
-    result = db.session.execute(db.select(Book).order_by(Book.id))
+    # result = db.session.execute(db.select(house).order_by(house.id))
+    result = db.session.execute(db.select(house).filter(house.id.in_(prop_list)).order_by(house.id))
+    
     # Use .scalars() to get the elements rather than entire rows from the database
-    all_books = result.scalars().all()
-    return render_template("prop_index.html", books=all_books)
+    
+    all_houses = result.scalars().all()
+    
+    return render_template("avail_prop_index.html", houses=all_houses)
 
+@app.route('/avail')
+def admin():
+
+    # READ ALL RECORDS
+    # Construct a query to select from the database. Returns the rows in the database
+    result = db.session.execute(db.select(house).order_by(house.id))
+    # result = db.session.execute(db.select(house).filter(house.id.in_(prop_list)).order_by(house.id))
+    # results = db.session.execute(db.select(Prop).order_by(Prop.id))
+    # Use .scalars() to get the elements rather than entire rows from the database
+    
+    all_houses = result.scalars().all()
+
+    return render_template("prop_index.html", houses=all_houses)
+    
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
         # CREATE RECORD
-        new_book = Book(
-            title=request.form["title"],
-            author=request.form["author"],
-            rating=request.form["rating"]
+        new_house = house(
+            id=request.form["id"],
+            SQFT=request.form["SQFT"],
+            Market_Add=request.form["Market_Add"]
         )
-        db.session.add(new_book)
+        db.session.add(new_house)
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template("add.html")
+    return render_template("prop_add.html")
 
+@app.route("/delete")
+def delete():
+    house_id = request.args.get('id')
+
+    # DELETE A RECORD BY ID
+    house_to_delete = db.get_or_404(house, house_id)
+    # Alternative way to select the book to delete.
+    # book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    db.session.delete(house_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 @app.route("/edit", methods=["GET", "POST"])
 def edit():
     if request.method == "POST":
         # UPDATE RECORD
-        book_id = request.form["id"]
-        book_to_update = db.get_or_404(Book, book_id)
-        book_to_update.rating = request.form["rating"]
+        house_id = request.form["id"]
+        house_to_update = db.get_or_404(house, house_id)
+        
+        if request.form['SQFT'] != '':
+            house_to_update.SQFT = request.form["SQFT"]
+        
+        if request.form['Market_Add'] != '':
+            house_to_update.Market_Add = request.form["Market_Add"]
+        
         db.session.commit()
         return redirect(url_for('home'))
-    book_id = request.args.get('id')
-    book_selected = db.get_or_404(Book, book_id)
-    return render_template("edit_rating.html", book=book_selected)
-
-
-@app.route("/delete")
-def delete():
-    book_id = request.args.get('id')
-
-    # DELETE A RECORD BY ID
-    book_to_delete = db.get_or_404(Book, book_id)
-    # Alternative way to select the book to delete.
-    # book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
-    db.session.delete(book_to_delete)
-    db.session.commit()
-    return redirect(url_for('home'))
-
+    
+    house_id = request.args.get('id')
+    house_selected = db.get_or_404(house, house_id)
+    return render_template("prop_edit_rating.html", house=house_selected)
 
 if __name__ == "__main__":
     app.run(debug=True)
